00287ffa579c1ffd354bab04e3cd5329ebc7c205
DFP <dfpcloud@gmail.com>
2018-03-12 17:05:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
-import tornado.httpserver
 import tornado.ioloop
-import tornado.options
 import tornado.web
-
-from tornado.options import define, options
-
-define("port", default=3090, help="0.0.0.0", type=int)
-
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
-
-class TestRoute(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Test Successful !!")
-
-def main():
-    tornado.options.parse_command_line()
-    application = tornado.web.Application([
+def make_app():
+    return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/test",TestRoute),
     ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
-
-
-if name == "main":
-    main()
